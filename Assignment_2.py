2493f209ebd4c098631739255aa1dfb6f46c8a29
Harsha Mathew <harshamathew111@gmail.com>
2022-12-10 00:52:15 +0000

    Update Assignment_2.py
    code for transposing and plotting bar graph for urban population

--- Assignment_2.py
+++ Assignment_2.py
@@ -9,10 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-agriculture = pd.read_csv("Agriculture.csv")
+"""agriculture = pd.read_csv("Agriculture.csv")
 print(agriculture)
-print(agriculture.dropna())
-
 agriculture = agriculture[['Country Name','1998','2003','2008','2013','2018']]
 agriculture1 = agriculture.set_index('Country Name').transpose()
 
@@ -24,8 +22,6 @@ agriculture2 = agriculture1[['United Arab Emirates','Australia','Pakistan','Braz
 print(agriculture2)
 
 CO2_emissions = pd.read_csv("CO2_emissions.csv")
-
-
 CO2_emissions = CO2_emissions[['Country Name','1998','2003','2008','2013','2018']]
 CO2_emissions1 = CO2_emissions.set_index('Country Name').transpose()
 
@@ -38,7 +34,7 @@ print(CO2_emissions2)
 
 
 coul_sources = pd.read_csv("coul_sources.csv")
-print(coul_sources.dropna())
+
 
 coul_sources = coul_sources[['Country Name','1998','2003','2008','2013','2018']]
 coul_sources1 = coul_sources.set_index('Country Name').transpose()
@@ -48,33 +44,47 @@ coul_sources2 = coul_sources1[['United Arab Emirates','Australia','Pakistan','Br
                              'Switzerland','China','Germany','Spain','United Kingdom',
                               'India','Japan','Croatia','Bangladesh']]
                              
-print(coul_sources2)
+print(coul_sources2.dropna(inplace = True))"""
                             
 urban_population = pd.read_csv("Urban_population.csv")
 print(urban_population.dropna())
-
+         
 urban_population = urban_population[['Country Name','1998','2003','2008','2013','2018']]
-urban_population1 = urban_population.set_index('Country Name').transpose()
-
+urban_population1 = urban_population.set_index('Country Name')
 print (urban_population1)
+urban_population1 = urban_population1[1:13]
+
+plt.figure(figsize=(13,14)) 
+plt.xlabel("Country Name")
+plt.ylabel("Ratio")
+plt.title("Urban Population",size=20)
+plt.bar(urban_population1.index, urban_population1["1998"])
+plt.xticks(rotation=90)
+plt.savefig('urban.png')
+plt.show()
+
 urban_population2 = urban_population1[['United Arab Emirates','Australia','Pakistan','Brazil',
                              'Switzerland','China','Germany','Spain','United Kingdom',
                               'India','Japan','Croatia','Bangladesh']]
-                             
-print(urban_population2.dropna())
 
+
+countryNames = ['United Arab Emirates','Australia','Pakistan','Brazil',
+                             'Switzerland','China','Germany','Spain','United Kingdom',
+                              'India','Japan','Croatia','Bangladesh']                       
+
+
+""""greenhouse_gas = greenhouse_gas[['Country Name','1998','2003','2008','2013','2018']]
+greenhouse_gas1 = greenhouse_gas.set_index('Country Name').transpose()
 greenhouse_gas = pd.read_csv("greenhouse_gas.csv")
 print(greenhouse_gas.dropna())
 
-greenhouse_gas = greenhouse_gas[['Country Name','1998','2003','2008','2013','2018']]
-greenhouse_gas1 = greenhouse_gas.set_index('Country Name').transpose()
 
 print (greenhouse_gas1)
 greenhouse_gas2 = greenhouse_gas1[['United Arab Emirates','Australia','Pakistan','Brazil',
                              'Switzerland','China','Germany','Spain','United Kingdom',
                               'India','Japan','Croatia','Bangladesh']]
                              
-print(greenhouse_gas2.dropna())
+print(greenhouse_gas2.dropna(inplace = True))"""
 
 
 
